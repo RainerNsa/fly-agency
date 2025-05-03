01a1821f205d87501af506bc8d15fce7a14fd0c1
Rainer Nsa <woldreamz@gmail.com>
2025-05-03 03:52:33 +0100

    Code Initialize

---
+++ app/routes.ts
@@ -0,0 +1,5 @@
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
+
+export default [
+    route('dashboard', 'routes/admin/dashboard.tsx'),
+] satisfies RouteConfig;
