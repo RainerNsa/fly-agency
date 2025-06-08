798fdae2d7faf1b220ac10c27dff1dd19fafc60c
Rainer Nsa <woldreamz@gmail.com>
2025-06-08 07:36:48 +0100

    chore: update gitignore and add appwrite configuration

---
+++ app/appwrite/client.ts
@@ -0,0 +1,9 @@
+export const appwriteConfig = {
+    endpointUrl: import.meta.env.VITE_APPWRITE_API_ENDPOINT,
+    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+    apiKey: import.meta.env.VITE_APPWRITE_API_KEY,
+    databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID, 
+    bucketId: import.meta.env.VITE_APPWRITE_BUCKET_ID,
+    userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+    tripCollectionId: import.meta.env.VITE_APPWRITE_TRIP_COLLECTION_ID,
+}
